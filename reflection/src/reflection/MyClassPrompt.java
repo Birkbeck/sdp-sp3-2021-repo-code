81477d04a89e74bb9f1e8ec6438f57b474481826
Keith Mannock <keith@dcs.bbk.ac.uk>
2021-01-22 14:26:55 +0000

    Command line reflection example from Command Prompt example

--- reflection/src/reflection/MyClassPrompt.java
+++ reflection/src/reflection/MyClassPrompt.java
@@ -3,15 +3,21 @@ package reflection;
 import java.lang.reflect.Method;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class MyClassPrompt {
   public static void main(String[] args) {
+    if (args.length == 0) {
+      System.err.println("MyClassPrompt <full class name including package>");
+      exit(-1);
+    }
     Class c;
     Scanner sc = null;
     try {
-      sc = new Scanner(System.in);
-      System.out.print("The name of the class: ");
-      String aclass = sc.next();
-      c = Class.forName(aclass);
+      //sc = new Scanner(System.in);
+      //System.out.print("The name of the class: ");
+      //String aclass = sc.next();
+      c = Class.forName(args[0]);
 
       System.out.println(c.getName());
       System.out.println(c.getSimpleName());
@@ -24,8 +30,8 @@ public class MyClassPrompt {
       }
     } catch (ClassNotFoundException e) {
       e.printStackTrace();
-    } finally {
-      sc.close();
+//    } finally {
+//      sc.close();
     }
   }
 }
