8a6ed0dc10b62b800fba054c3969f356e17371b6
bkrmalick <bkrmalick@hotmail.com>
2021-01-23 20:01:16 +0000

    fixed array names

--- Week-Two-Examples/solid-two/src/AreaCalculator.java
+++ Week-Two-Examples/solid-two/src/AreaCalculator.java
@@ -1,17 +1,17 @@
 import java.util.Arrays;
 
 public class AreaCalculator {
-  public static double totalArea(Rectangle[] arrOfShapes){
+  public static double totalArea(Rectangle[] arrOfRect){
     var sum = 0.0;
-    for (var shape : arrOfShapes){
+    for (var shape : arrOfRect){
       sum += shape.area();
     }
     return sum;
   }
 
-  public static double totalArea(Circle[] arrOfShapes){
+  public static double totalArea(Circle[] arrOfCirc){
     var sum = 0.0;
-    for (var shape : arrOfShapes){
+    for (var shape : arrOfCirc){
       sum += shape.area();
     }
     return sum;
